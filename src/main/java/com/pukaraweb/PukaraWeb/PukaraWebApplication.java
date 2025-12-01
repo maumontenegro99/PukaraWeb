1e520d984e91f20022902f6d504526c9be896ffa
Mauricio Montenegro <mau.montenegro@duocuc.cl>
2025-12-01 14:52:20 -0300

    Primer commit: Estructura base Spring Boot

---
+++ src/main/java/com/pukaraweb/PukaraWeb/PukaraWebApplication.java
@@ -0,0 +1,13 @@
+package com.pukaraweb.PukaraWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PukaraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PukaraWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pukaraweb/PukaraWeb/PukaraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pukaraweb.PukaraWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PukaraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
